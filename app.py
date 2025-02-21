c89bc88e7d89fd6c42ab037431bfc0b0d96c5be8
JARP <andersonandy046@gmail.com>
2025-02-22 00:01:00 +0800

    update

--- app.py
+++ app.py
@@ -8,7 +8,19 @@ app.secret_key = "gdwadwad"
 
 @app.route('/')
 def landing():
-    return render_template('patient.html')
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 
 
